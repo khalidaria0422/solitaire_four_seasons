9647fcb0520f8a3db5477362e4c5491cca250cea
Khalid Aria <khalidaria0422@gmail.com>
2025-05-01 15:25:20 -0400

    added a font loading and retrieving class

---
+++ src/fonts.cpp
@@ -0,0 +1,16 @@
+#include "../include/fonts.h"
+#include <SFML/Graphics/Font.hpp>
+#include <iostream>
+#include <filesystem>
+
+//---------- init consts ----------//
+const std::string rootPath = std::filesystem::current_path().string(); // path to the root project dir
+
+//---------- init statics ----------//
+sf::Font Font::Reg;
+sf::Font Font::Bold;
+
+void Font::loadFont() {
+  if (!Reg.loadFromFile(rootPath + "/assets/fonts/NotoSans-Regular.ttf") || 
+      !Bold.loadFromFile(rootPath + "/assets/fonts/NotoSans-Bold.ttf")) { std::cerr << "Failed to load the fonts." << std::endl; }
+}
